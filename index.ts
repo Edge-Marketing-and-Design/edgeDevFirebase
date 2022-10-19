2d84e6e181e7d0ca335db5262f98dae8676cf791
Seth Fischer <seth@itsgeek2me.net>
2022-10-18 21:53:00 -0700

    update

--- index.ts
+++ index.ts
@@ -29,7 +29,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = getAuth();
 const db = getFirestore(app);
 
 export const signIn = async (credentials: Credentials): Promise<object> => {
