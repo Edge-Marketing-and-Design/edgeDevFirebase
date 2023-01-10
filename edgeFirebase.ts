ab38ecbebf37fa970af429546f306bca84676f35
Seth Fischer <seth@itsgeek2me.net>
2023-01-09 21:37:22 -0800

    update

--- edgeFirebase.ts
+++ edgeFirebase.ts
@@ -1133,7 +1133,7 @@ export const EdgeFirebase = class {
     });
   }
 
-  public startUsersSnapshot = (collectionPath = ''): void => {
+  public startUsersSnapshot = async(collectionPath = ''): Promise<void> => {
     this.stopUsersSnapshot();
     for (const collectionPathCheck of this.user.canAssignCollectionPaths) {
       
@@ -1147,7 +1147,7 @@ export const EdgeFirebase = class {
             collectionPathCheck
           )
         )
-        const rolesUnsubscribe = onSnapshot(q, (querySnapshot) => {
+        const rolesUnsubscribe = await onSnapshot(q, (querySnapshot) => {
           const items = {};
           querySnapshot.forEach((doc) => {
             const user = doc.data();
@@ -1177,7 +1177,7 @@ export const EdgeFirebase = class {
           )
         )
 
-        const specialPermissionsunsubscribe = onSnapshot(q, (querySnapshot) => {
+        const specialPermissionsunsubscribe = await onSnapshot(q, (querySnapshot) => {
           const items = {};
           querySnapshot.forEach((doc) => {
             const user = doc.data();
