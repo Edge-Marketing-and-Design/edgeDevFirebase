fc2d68338a0e0f30220f0477004307113c39e0eb
sethjfischer <seth@itsgeek2me.net>
2023-04-13 14:55:39 -0700

    updates

--- src/postinstall.sh
+++ src/postinstall.sh
@@ -3,15 +3,32 @@
 # Check if the destination file exists
 if [ ! -f ./firestore.rules ]; then
   # If the file does not exist, create it and add the rules_version line
-  echo 'rules_version = '\''2'\'';' > ./firestore.rules;
-else
-  # If the file exists, add the rules_version line at the beginning using sed
-  sed -i '1s/^/rules_version = \\'\''2\\'\'';\\n/' ./firestore.rules;
+  echo "rules_version = '2';" > ./firestore.rules;
 fi
 
+[ "$(tail -c1 ./firestore.rules)" != "" ] && echo "" >> ./firestore.rules
+
 # Extract the code block from the source file and append it to the destination file
-awk '/\/\/ #EDGE FIREBASE RULES START/,/\/\/ #EDGE FIREBASE RULES END/' ./src/firestore.rules | \
-  sed 's/\\/\\\\/g; s/&/\\&/g' | \
-  sed -e 's/\"/\\\\\"/g' -e 's/\//\\\\\//g' | \
+sed -i.backup '/\/\/ #EDGE FIREBASE RULES START/,/\/\/ #EDGE FIREBASE RULES END/d' ./firestore.rules
+
+awk '/\/\/ #EDGE FIREBASE RULES START/,/\/\/ #EDGE FIREBASE RULES END/' ./node_modules/@edgedev/firebase/src/firestore.rules | \
+  sed '1d;$d' | \
   sed -e '1s/^/\/\/ #EDGE FIREBASE RULES START\n/' -e '$s/$/\n\/\/ #EDGE FIREBASE RULES END/' \
   >> ./firestore.rules;
+
+if [ ! -f ./functions/index.js ]; then
+  mkdir -p ./functions
+  echo "const functions = require('firebase-functions');" > ./functions/index.js;
+  echo "const admin = require('firebase-admin');" >> ./functions/index.js;
+  echo "admin.initializeApp();" >> ./functions/index.js;
+  echo "const db = admin.firestore();" >> ./functions/index.js;
+fi   
+
+[ "$(tail -c1 ./firestore.rules)" != "" ] && echo "" >> ./functions/index.js
+
+sed -i.backup '/\/\/ START @edge\/firebase functions/,/\/\/ END @edge\/firebase functions/d' ./functions/index.js
+
+awk '/\/\/ START @edge\/firebase functions/,/\/\/ END @edge\/firebase functions/' ./node_modules/@edgedev/firebase/src/functions.js | \
+  sed '1d;$d' | \
+  sed -e '1s/^/\/\/ START @edge\/firebase functions\n/' -e '$s/$/\n\/\/ END @edge\/firebase functions/' \
+  >> ./functions/index.js;
