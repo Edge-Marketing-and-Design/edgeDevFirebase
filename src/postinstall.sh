899fa279a3a76c666723f9542ab03920159497d5
sethjfischer <seth@itsgeek2me.net>
2023-04-14 15:52:02 -0700

    bug fix

--- src/postinstall.sh
+++ src/postinstall.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+# Resolve the symlink and get the actual directory path
+actual_dir=$(readlink -f $(dirname $0))
+# Get the project root directory by removing the node_modules directory path
+project_root=$(echo "$actual_dir" | awk '{gsub(/node_modules.*$/, ""); print}')
+
+
 # Check if the destination file exists
-if [ ! -f ./firestore.rules ]; then
+if [ ! -f "$project_root/firestore.rules" ]; then
   # If the file does not exist, create it and add the rules_version line
-  echo "rules_version = '2';" > ./firestore.rules;
+  echo "rules_version = '2';" > "$project_root/firestore.rules";
 fi
 
-[ "$(tail -c1 ./firestore.rules)" != "" ] && echo "" >> ./firestore.rules
+[ "$(tail -c1 $project_root/firestore.rules)" != "" ] && echo "" >> "$project_root/firestore.rules"
 
-# Extract the code block from the source file and append it to the destination file
-sed -i.backup '/\/\/ #EDGE FIREBASE RULES START/,/\/\/ #EDGE FIREBASE RULES END/d' ./firestore.rules
+# # Extract the code block from the source file and append it to the destination file
+sed -i.backup '/\/\/ #EDGE FIREBASE RULES START/,/\/\/ #EDGE FIREBASE RULES END/d' "$project_root/firestore.rules"
 
-awk '/\/\/ #EDGE FIREBASE RULES START/,/\/\/ #EDGE FIREBASE RULES END/' ./node_modules/@edgedev/firebase/src/firestore.rules | \
+awk '/\/\/ #EDGE FIREBASE RULES START/,/\/\/ #EDGE FIREBASE RULES END/' ./src/firestore.rules | \
   sed '1d;$d' | \
   sed -e '1s/^/\/\/ #EDGE FIREBASE RULES START\n/' -e '$s/$/\n\/\/ #EDGE FIREBASE RULES END/' \
-  >> ./firestore.rules;
+  >> "$project_root/firestore.rules";
 
 if [ ! -f ./functions/index.js ]; then
   mkdir -p ./functions
@@ -24,11 +30,11 @@ if [ ! -f ./functions/index.js ]; then
   echo "const db = admin.firestore();" >> ./functions/index.js;
 fi   
 
-[ "$(tail -c1 ./firestore.rules)" != "" ] && echo "" >> ./functions/index.js
+[ "$(tail -c1 $project_root/functions/index.js)" != "" ] && echo "" >> "$project_root/functions/index.js"
 
-sed -i.backup '/\/\/ START @edge\/firebase functions/,/\/\/ END @edge\/firebase functions/d' ./functions/index.js
+sed -i.backup '/\/\/ START @edge\/firebase functions/,/\/\/ END @edge\/firebase functions/d' "$project_root/functions/index.js"
 
-awk '/\/\/ START @edge\/firebase functions/,/\/\/ END @edge\/firebase functions/' ./node_modules/@edgedev/firebase/src/functions.js | \
+awk '/\/\/ START @edge\/firebase functions/,/\/\/ END @edge\/firebase functions/' ./src/functions.js | \
   sed '1d;$d' | \
   sed -e '1s/^/\/\/ START @edge\/firebase functions\n/' -e '$s/$/\n\/\/ END @edge\/firebase functions/' \
-  >> ./functions/index.js;
+  >> "$project_root/functions/index.js";
