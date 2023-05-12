d3b95685af9d72b5de098adaf8d55ae987eba6fe
sethjfischer <seth@itsgeek2me.net>
2023-05-11 17:42:06 -0700

    updates

--- src/postinstall.sh
+++ src/postinstall.sh
@@ -22,12 +22,12 @@ awk '/\/\/ #EDGE FIREBASE RULES START/,/\/\/ #EDGE FIREBASE RULES END/' ./src/fi
   sed -e '1s/^/\/\/ #EDGE FIREBASE RULES START\n/' -e '$s/$/\n\/\/ #EDGE FIREBASE RULES END/' \
   >> "$project_root/firestore.rules";
 
-if [ ! -f ./functions/index.js ]; then
-  mkdir -p ./functions
-  echo "const functions = require('firebase-functions');" > ./functions/index.js;
-  echo "const admin = require('firebase-admin');" >> ./functions/index.js;
-  echo "admin.initializeApp();" >> ./functions/index.js;
-  echo "const db = admin.firestore();" >> ./functions/index.js;
+if [ ! -f "$project_root/functions/index.js" ]; then
+  mkdir -p "$project_root/functions"
+  echo "const functions = require('firebase-functions');" > "$project_root/functions/index.js";
+  echo "const admin = require('firebase-admin');" >> "$project_root/functions/index.js";
+  echo "admin.initializeApp();" >> "$project_root/functions/index.js";
+  echo "const db = admin.firestore();" >> "$project_root/functions/index.js";
 fi   
 
 [ "$(tail -c1 $project_root/functions/index.js)" != "" ] && echo "" >> "$project_root/functions/index.js"
