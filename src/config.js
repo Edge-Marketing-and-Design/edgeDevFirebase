38738047582b40e06712c54f83f612c3d10645d2
Seth Fischer <seth@itsgeek2me.net>
2023-10-11 01:52:59 -0700

    updates

--- src/config.js
+++ src/config.js
@@ -4,12 +4,28 @@ const admin = require('firebase-admin')
 admin.initializeApp()
 
 const { onCall, HttpsError } = require('firebase-functions/v2/https')
+const { onSchedule } = require('firebase-functions/v2/scheduler')
+const {
+  onDocumentWritten,
+  onDocumentCreated,
+  onDocumentUpdated,
+  onDocumentDeleted,
+  Change,
+  FirestoreEvent,
+} = require('firebase-functions/v2/firestore')
 const { logger } = require('firebase-functions/v2')
 const { getFirestore } = require('firebase-admin/firestore')
 const twilio = require('twilio')
 const db = getFirestore()
 
 module.exports = {
+  onSchedule,
+  onDocumentWritten,
+  onDocumentCreated,
+  onDocumentUpdated,
+  onDocumentDeleted,
+  Change,
+  FirestoreEvent,
   onCall,
   HttpsError,
   logger,
@@ -19,3 +35,4 @@ module.exports = {
   twilio,
   db,
 }
+
